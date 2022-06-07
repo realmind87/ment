6f1a31f641a1d47ab04c3a319e818b988f9d6059
realmind87 <realmind87@naver.com>
2022-06-07 13:49:52 +0900

    commit

--- back/app.js
+++ back/app.js
@@ -29,7 +29,7 @@ passportConfig();
 app.use(express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET, { sameSite: "none", secure: true }));
 app.use(
   session({
     saveUninitialized: false,
